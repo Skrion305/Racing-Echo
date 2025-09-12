230f6930973784288760e7d2224c25e29298ddcb
gorgonzollo <kirasir2006@gmail.com>
2025-09-12 19:14:47 +0300

    2DOF

---
+++ F1/Assets/LogitechInputSamples.cs
@@ -0,0 +1,15 @@
+using LogitechG29.Sample.Input;
+using UnityEngine;
+
+public class LogitechInputSamples : MonoBehaviour
+{
+    [SerializeField] InputControllerReader icr;
+    private void OnEnable()
+    {
+
+    }
+    private void OnDisable()
+    {
+        
+    }
+}
