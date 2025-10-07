7d7c8e559f27756187418fe9496d810edcf34ab8
Skrion305 <kirasir2006@gmail.com>
2025-10-08 02:05:35 +0300

    1

--- F1/Assets/Sample/CarControllerSample.cs
+++ F1/Assets/Sample/CarControllerSample.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using LogitechG29.Sample.Input;
 using UnityEngine;
+using static CarControllerSample;
 
 #endregion
 
@@ -45,7 +46,18 @@ public class CarControllerSample : MonoBehaviour
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
             }
+            VisualWheel(axleInfo.leftWheel);
+            VisualWheel(axleInfo.rightWheel);
         }
+    }
+    public void VisualWheel(WheelCollider collider)
+    {
+        Transform visualWheel = collider.transform.GetChild(0);
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
     }
 
     [Serializable]
