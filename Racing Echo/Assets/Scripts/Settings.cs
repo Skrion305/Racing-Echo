df77c5fdc1b8b61583560b9fe589620f3bed2e30
Kirill Sirotkin <kirasir2006@gmail.com>
2025-12-13 00:48:56 +0300

    Checkpoints

--- Racing Echo/Assets/Scripts/Settings.cs
+++ Racing Echo/Assets/Scripts/Settings.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public static class Settings
 {
     public static string gameMode = "Гонка";
-    public static int raceTrack = 2;
+    public static int raceTrack = 3;
     public static float record = 0;
 }
