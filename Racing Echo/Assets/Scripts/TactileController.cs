ccf289c9d820895af005ae9ad7d281646b1cae72
Kirill Sirotkin <kirasir2006@gmail.com>
2025-12-18 20:56:48 +0300

    Update TactileController.cs

--- Racing Echo/Assets/Scripts/TactileController.cs
+++ Racing Echo/Assets/Scripts/TactileController.cs
@@ -6,15 +6,12 @@ public class TactileController : MonoBehaviour
 {
     [SerializeField] InputControllerReader inputControllerReader;
     Rigidbody carRigidbody;
-    //bool isGrounded = true;
     void Start()
     {
         carRigidbody = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        //CheckEngineVibration();
-        //CheckSuspensionVibration();
         CheckBrakingFeedback();
     }
     void OnCollisionEnter(Collision collision)
@@ -28,50 +25,6 @@ public class TactileController : MonoBehaviour
             PlayCollisionImpact();
         }
     }
-    /*void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Road"))
-        {
-            isGrounded = true;
-        }
-    }*/
-    /*void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Road"))
-        {
-            isGrounded = false;
-        }
-        else if (other.CompareTag("Springboard"))
-        {
-            PlayJumpLaunch();
-        }
-    }*/
-    /*void CheckEngineVibration()
-    {
-        float engineIntensity;
-        float throttleInput = Mathf.Abs(inputControllerReader.Throttle);
-        float brakeInput = Mathf.Abs(inputControllerReader.Brake);
-        if (throttleInput > brakeInput)
-        {
-            engineIntensity = Mathf.Clamp01(inputControllerReader.Throttle);
-        }
-        else
-        {
-            engineIntensity = Mathf.Clamp01(inputControllerReader.Brake);
-        }
-        if (engineIntensity > 0.1f)
-        {
-            BhapticsLibrary.Play(eventId: "engine_vibration", startMillis: 0, intensity: engineIntensity * 0.7f, duration: 100, angleX: 0, offsetY: 0);
-        }
-    }*/
-    /*void CheckSuspensionVibration()
-    {
-        float speed = carRigidbody.linearVelocity.magnitude;
-        if (isGrounded && (speed > 3f))
-        {
-            BhapticsLibrary.Play(eventId: "landing_impact", startMillis: 0, intensity: Mathf.Clamp01(speed / 20f), duration: 300, angleX: 0, offsetY: 0);
-        }
-    }*/
     void CheckBrakingFeedback()
     {
         if (inputControllerReader.Brake > 0.8f)
@@ -91,10 +44,6 @@ public class TactileController : MonoBehaviour
     {
         BhapticsLibrary.Play(eventId: "car_accident", startMillis: 0, intensity: 1, duration: 600, angleX: 0, offsetY: 0);
     }
-    /*public void PlayJumpLaunch()
-    {
-        BhapticsLibrary.Play(eventId: "jump_liftoff", startMillis: 100, intensity: 0.7f, duration: 300, angleX: 0, offsetY: 0);
-    }*/
     void OnDestroy()
     {
         BhapticsLibrary.StopAll();
